71c97752e68cf8ae10d955b0727608691c432d96
Leo <leodelmas31@gmail.com>
2024-09-08 11:54:46 +0200

    liste

--- src/main/java/com/springTest/spring_first_app/StorageController.java
+++ src/main/java/com/springTest/spring_first_app/StorageController.java
@@ -1,5 +1,8 @@
 package com.springTest.spring_first_app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,16 @@ public class StorageController {
 	@GetMapping("/jean")
 	public Storage getStorage() {
 		return new Storage("jean", "cule");
+	}
+
+	@GetMapping("/liste")
+	public List<Storage> getMultipleStorage() {
+		List<Storage> multiStorage = new ArrayList<>();
+		Storage jean = new Storage("jean", "cule");
+		Storage paul = new Storage("paul", "poule");
+		multiStorage.add(jean);
+		multiStorage.add(paul);
+		multiStorage.add(new Storage("phil", "hémon"));
+		return multiStorage;
 	}
 }
